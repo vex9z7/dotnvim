841497dfa7a95a4323ae3b9d1956a1ee6bf88494
vex9z7 <face.tubas0p@icloud.com>
2025-06-12 17:36:00 +0900

    pin the lazy.nvim version to v11.17.1

--- lua/lazy-bootstrap.lua
+++ lua/lazy-bootstrap.lua
@@ -3,7 +3,7 @@
 local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
   local lazyrepo = 'https://github.com/folke/lazy.nvim.git'
-  local out = vim.fn.system { 'git', 'clone', '--filter=blob:none', '--branch=stable', lazyrepo, lazypath }
+  local out = vim.fn.system { 'git', 'clone', '--filter=blob:none', '--branch=v11.17.1','--depth','1', lazyrepo, lazypath }
   if vim.v.shell_error ~= 0 then
     error('Error cloning lazy.nvim:\n' .. out)
   end
