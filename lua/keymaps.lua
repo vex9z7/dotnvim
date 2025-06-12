f0d01e26accd6ed78a2e445c118e16ac8f3fa750
vex9z7 <face.tubas0p@icloud.com>
2025-06-12 16:01:24 +0900

    fix: revert hl to highlight for nvim 10.0.*

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -42,12 +42,12 @@ vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper win
 
 -- Highlight when yanking (copying) text
 --  Try it with `yap` in normal mode
---  See `:help vim.hl.on_yank()`
+--  See `:help vim.hightlight.on_yank()`
 vim.api.nvim_create_autocmd('TextYankPost', {
   desc = 'Highlight when yanking (copying) text',
   group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
   callback = function()
-    vim.hl.on_yank()
+    vim.highlight.on_yank()
   end,
 })
 
