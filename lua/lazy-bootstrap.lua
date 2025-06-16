f46376e50140c24a07a7894408b74355f6d29627
vex9z7 <face.tubas0p@icloud.com>
2025-06-16 15:58:18 +0900

    fix: pull the history commits of lazy.nvim

--- lua/lazy-bootstrap.lua
+++ lua/lazy-bootstrap.lua
@@ -3,14 +3,20 @@
 local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
   local lazyrepo = 'https://github.com/folke/lazy.nvim.git'
-  local out = vim.fn.system { 'git', 'clone', '--filter=blob:none', '--branch=v11.17.1','--depth','1', lazyrepo, lazypath }
+  local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=v11.17.1", lazyrepo, lazypath })
   if vim.v.shell_error ~= 0 then
-    error('Error cloning lazy.nvim:\n' .. out)
+    vim.api.nvim_echo({
+      { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+      { out, "WarningMsg" },
+      { "\nPress any key to exit..." },
+    }, true, {})
+    vim.fn.getchar()
+    os.exit(1)
   end
 end
 
 ---@type vim.Option
 local rtp = vim.opt.rtp
-rtp:prepend(lazypath)
+vim.opt.rtp:prepend(lazypath)
 
 -- vim: ts=2 sts=2 sw=2 et
