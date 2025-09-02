777abbf47dc3858a83a9c49d0094163ea6d2cc97
vex9z7 <face.tubas0p@icloud.com>
2025-09-02 15:54:06 +0900

    feat(options): set tab witdh and expand tab defaults

--- lua/options.lua
+++ lua/options.lua
@@ -71,4 +71,7 @@ vim.o.scrolloff = 10
 -- See `:help 'confirm'`
 vim.o.confirm = true
 
--- vim: ts=2 sts=2 sw=2 et
+vim.opt.tabstop = 2
+vim.opt.softtabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.expandtab = true
