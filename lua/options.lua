268a5d3b6dff30812bfa01010cbcd8d93d281a15
vex9z7 <face.tubas0p@icloud.com>
2025-09-12 14:33:20 +0900

    feat(options): enable relative line numbers by default

--- lua/options.lua
+++ lua/options.lua
@@ -3,8 +3,9 @@
 -- NOTE: You can change these options as you wish!
 --  For more options, you can see `:help option-list`
 
--- Make line numbers default
+-- Make line numbers and relative number default
 vim.o.number = true
+vim.o.relativenumber = true
 -- You can also add relative line numbers, to help with jumping.
 --  Experiment for yourself to see if you like it!
 -- vim.o.relativenumber = true
