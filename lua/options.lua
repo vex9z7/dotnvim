b4be93cbdcbdda041105437b76794ccb284c0658
vex9z7 <face.tubas0p@icloud.com>
2025-09-12 14:51:03 +0900

    feat(options): configure persisten undo directory and increase undo history

--- lua/options.lua
+++ lua/options.lua
@@ -29,6 +29,11 @@ vim.o.breakindent = true
 
 -- Save undo history
 vim.o.undofile = true
+vim.o.undodir = vim.env.HOME .. '/.config/nvim/undo'
+
+-- number of undo saved
+vim.o.undolevels = 10000
+vim.o.undoreload = 10000
 
 -- Case-insensitive searching UNLESS \C or one or more capital letters in the search term
 vim.o.ignorecase = true
