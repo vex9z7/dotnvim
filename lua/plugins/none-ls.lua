8f8c719d07de98f5fc316473829393b78d3a0702
vex9z7 <face.tubas0p@icloud.com>
2025-09-22 15:26:32 +0900

    refactor(none-ls): simplify plugin table structure

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -30,62 +30,60 @@ local function cbfmtSourceFactory()
 end
 
 return {
-  {
-    'nvimtools/none-ls.nvim',
-    dependencies = { 'nvim-lua/plenary.nvim', 'nvimtools/none-ls-extras.nvim', 'davidmh/cspell.nvim' },
-    config = function()
-      local null_ls = require 'null-ls'
+  'nvimtools/none-ls.nvim',
+  dependencies = { 'nvim-lua/plenary.nvim', 'nvimtools/none-ls-extras.nvim', 'davidmh/cspell.nvim' },
+  config = function()
+    local null_ls = require 'null-ls'
 
-      vim.diagnostic.config {
-        underline = {
-          severity = {
-            min = vim.diagnostic.severity.HINT, -- INFO: underline for all severity
-          },
+    vim.diagnostic.config {
+      underline = {
+        severity = {
+          min = vim.diagnostic.severity.HINT, -- INFO: underline for all severity
         },
-      }
+      },
+    }
 
-      null_ls.setup {
-        sources = {
-          -- see at https://github.com/nvimtools/none-ls-extras.nvim/tree/main
-          require 'none-ls.diagnostics.eslint_d',
-          require 'none-ls.code_actions.eslint_d',
+    null_ls.setup {
+      sources = {
+        -- see at https://github.com/nvimtools/none-ls-extras.nvim/tree/main
+        require 'none-ls.diagnostics.eslint_d',
+        require 'none-ls.code_actions.eslint_d',
 
-          -- see at https://github.com/davidmh/cspell.nvim
-          require('cspell.diagnostics').with {
-            diagnostic_config = {
-              -- see at :help vim.diagnostic.config()
-              underline = true,
-              virtual_text = true,
-              signs = false,
-              update_in_insert = false,
-              severity_sort = true,
-            },
-
-            diagnostics_postprocess = function(diagnostic)
-              -- see :help diagnostic-severity
-              diagnostic.severity = vim.diagnostic.severity.HINT
-            end,
-
-            -- TODO: properly config the cspell.json path
-            -- config = {
-            --   find_json = function(cwd)
-            --     local CSPELL_FALLBACK_CONFIG_PATH = vim.fn.expand '~/.config/nvim/tool-config/cspell.json'
-            --     return CSPELL_FALLBACK_CONFIG_PATH
-            --   end,
-            -- },
+        -- see at https://github.com/davidmh/cspell.nvim
+        require('cspell.diagnostics').with {
+          diagnostic_config = {
+            -- see at :help vim.diagnostic.config()
+            underline = true,
+            virtual_text = true,
+            signs = false,
+            update_in_insert = false,
+            severity_sort = true,
           },
 
-          require 'cspell.code_actions',
+          diagnostics_postprocess = function(diagnostic)
+            -- see :help diagnostic-severity
+            diagnostic.severity = vim.diagnostic.severity.HINT
+          end,
 
-          -- TODO: migrate to conform
-          -- -- shell
-          -- formatting.shfmt,
-
-          -- TODO: migrate to inject formatting of conform
-          -- Markdown
-          cbfmtSourceFactory(),
+          -- TODO: properly config the cspell.json path
+          -- config = {
+          --   find_json = function(cwd)
+          --     local CSPELL_FALLBACK_CONFIG_PATH = vim.fn.expand '~/.config/nvim/tool-config/cspell.json'
+          --     return CSPELL_FALLBACK_CONFIG_PATH
+          --   end,
+          -- },
         },
-      }
-    end,
-  },
+
+        require 'cspell.code_actions',
+
+        -- TODO: migrate to conform
+        -- -- shell
+        -- formatting.shfmt,
+
+        -- TODO: migrate to inject formatting of conform
+        -- Markdown
+        cbfmtSourceFactory(),
+      },
+    }
+  end,
 }
