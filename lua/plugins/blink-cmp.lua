f2fb22407f9c875bbb42b6eab5191fc2c784fc88
vex9z7 <face.tubas0p@icloud.com>
2025-10-02 21:48:57 +0900

    feat(blink-cmp): add friendly-snippets support for LuaSnip

--- lua/plugins/blink-cmp.lua
+++ lua/plugins/blink-cmp.lua
@@ -5,31 +5,7 @@ return {
     version = '1.*',
     dependencies = {
       -- Snippet Engine
-      {
-        'L3MON4D3/LuaSnip',
-        version = '2.*',
-        build = (function()
-          -- Build Step is needed for regex support in snippets.
-          -- This step is not supported in many windows environments.
-          -- Remove the below condition to re-enable on windows.
-          if vim.fn.has 'win32' == 1 or vim.fn.executable 'make' == 0 then
-            return
-          end
-          return 'make install_jsregexp'
-        end)(),
-        dependencies = {
-          -- `friendly-snippets` contains a variety of premade snippets.
-          --    See the README about individual language/framework/plugin snippets:
-          --    https://github.com/rafamadriz/friendly-snippets
-          -- {
-          --   'rafamadriz/friendly-snippets',
-          --   config = function()
-          --     require('luasnip.loaders.from_vscode').lazy_load()
-          --   end,
-          -- },
-        },
-        opts = {},
-      },
+      'L3MON4D3/LuaSnip',
       'folke/lazydev.nvim',
     },
     --- @module 'blink.cmp'
@@ -97,5 +73,30 @@ return {
       signature = { enabled = true },
     },
   },
+  {
+    'L3MON4D3/LuaSnip',
+    version = '2.*',
+    build = (function()
+      -- Build Step is needed for regex support in snippets.
+      -- This step is not supported in many windows environments.
+      -- Remove the below condition to re-enable on windows.
+      if vim.fn.has 'win32' == 1 or vim.fn.executable 'make' == 0 then
+        return
+      end
+      return 'make install_jsregexp'
+    end)(),
+    dependencies = {
+      -- `friendly-snippets` contains a variety of premade snippets.
+      'rafamadriz/friendly-snippets',
+    },
+    opts = {},
+  },
+  {
+    -- INFO: See the README about individual language/framework/plugin snippets:
+    -- https://github.com/rafamadriz/friendly-snippets
+    'rafamadriz/friendly-snippets',
+    config = function()
+      require('luasnip.loaders.from_vscode').lazy_load()
+    end,
+  },
 }
--- vim: ts=2 sts=2 sw=2 et
